b37f58c36376c42bd26ebd558906d8887a721c58
Маньковський Олександр <117378642+RootSasha@users.noreply.github.com>
2026-06-22 12:05:18 +0300

    Delete skillswap_core_backend directory

--- skillswap_core_backend/app/core/seed_skills.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO skills (name) VALUES
-    ('Python'), ('JavaScript'), ('TypeScript'), ('Go'), ('Rust'),
-    ('React'), ('Vue'), ('Figma'), ('Photoshop'), ('Illustrator'),
-    ('Copywriting'), ('SEO'), ('Targeting'), ('SMM'), ('UI/UX Design'),
-    ('DevOps'), ('Docker'), ('PostgreSQL'), ('Redis'), ('Machine Learning')
-ON CONFLICT (name) DO NOTHING;
